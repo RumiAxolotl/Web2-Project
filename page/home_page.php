25ca3eb89651ad12eeaa941bdd2b0db649bb143e
RumiAxolotl <rumiaxolotl@gmail.com>
2023-05-18 18:14:44 +0700

    Update home_page.php

--- page/home_page.php
+++ page/home_page.php
@@ -6,12 +6,11 @@
         <div class="container">
 <div class="row">
                 <div class="col-2">
-                    <h1>Săn ngay giày khủng<br> 500+ mẫu các loại</h1>
-                    <p>TGX shop luôn luôn mang đến những mẫu giày hiện đại và tốt nhất cho người dùng, đa dạng về màu , size và kích thước,... Hãy đến với chúng tôi</p>
+                    <p>Bakery shop là tiệm bánh cung cấp cho người mua những loại bánh đa dạng về hương vị và... Hãy đến với chúng tôi</p>
                     <a href="index.php?action=productclassification" class="btn">Khám phá ngay &#8594;</a>
                 </div>
                 <div class="col-2">
-                    <img src="images/banner2.png">
+                    <img src="images/logo.png">
                 </div>
             </div>
             </div>
@@ -20,13 +19,13 @@
         <div class="small-container">
             <div class="row">
                 <div class="col-3">
-                    <img src="images/category-1.jpg">
+                    <img src="images/logo.png">
                 </div>
                 <div class="col-3">
-                    <img src="images/category-2.jpg">
+                    <img src="images/logo.png">
                 </div>
                 <div class="col-3">
-                    <img style= "width: 309px;height: 339px" src="images/category-3.jpg">
+                    <img style= "width: 309px;height: 339px" src="images/logo.png">
                 </div>
 
             </div>
@@ -38,7 +37,6 @@
             <h1 class="lg-title">Sản Phẩm Hot</h1>
 
             <div class="product-items">
-                <!-- Sản phẩm đơn lẻ Mau giày Adidas Ultra Boost 4.0 Parley được thiết kế mang nét đậm chất thể thao, năng động cho bạn trải nghiệm tuyệt vời nhất khi đi lên chân với trọng lượng của đôi giày cực nhẹ có khả năng tạo lực đẩy này có phần thân trên làm từ vải dệt không đường may, tạo độ co giãn mà vẫn chắc chắn trong khi chạy. Đôi Ultra Boost luôn nằm trong top những Mau giày chạy bộ được yêu thích nhất trong nhiều năm. Giày Adidas Ultra Boost 4.0 Parley [AC8205] có thiết kế dùng công nghệ Primeknit 360 upper Giày Ultra Boost 4.0 có thiết kế ôm sát chân, công nghệ vải Primeknit 360 upper (thân trên) được làm bằng vải dệt Primeknit với độ ôm được thiết kế đặc biệt để nâng đỡ có định hướng. Một đôi giày tuyệt vời để kết hợp cùng trang phục khác nhau tạo nên set đồ ấn tượng cho người đi Ultra Boost. Không chỉ cho nam giới, mà đây còn là Mau giày adidas cho nữ đi cũng rất hợp.-->
                 <div class="product">
                     <div class="product-content">
                       <a  href="index.php?id=3&action=detail">
@@ -173,49 +171,6 @@
                
                    
             </div> 
-        </div>
-    </div>
-
-    <div class="all-product-2">
-            <div class="follow">
-              <h2>FOLLOW <br> BUBU SHOP ON INSTAGRAM</h2>
-            </div>
-            <div class="img-4">
-              <img src="images/product2/9.png" alt="">
-              <img src="images/product2/10.png" alt="">
-              <img src="images/product2/11.png" alt="">
-              <img src="images/product2/12.png" alt="">
-              <img src="images/product2/13.png" alt="">
-            </div>  
-            <div class="over">
-              <div class="letter">
-                <hr width="3%" size="5" color="orange">
-                <h3><i>Giao hàng tận nơi</i></h3>
-              </div>
-              <div class="letter">
-                <hr width="3%" size="5" color="orange">
-                <h3><i>Hoàn trả dễ dàng trong 30 ngày</i></h3>
-              </div>
-              <div class="letter">
-                <hr width="3%" size="5" color="orange">
-                <h3><i>Bảo hành 12 tháng</i></h3>
-              </div>
-            </div>
-          </div>
-    <!-- ------------ offer -------------- -->
-    <div class="offer">
-        <div class="small-container">
-            <div class="row">
-                <div class="col-2"><img src="images/banner1.png" class="offer-img"> </div>
-                <div class="col-2">
-                    <p>Duy nhất có ở tại TGX Shop</p>
-                    <h2>Siêu sale giáng sinh 24/12 - 25/12</h2>
-                    <small>
-                        Đến với TGX Shop để có được những đôi giày thật sang trọng, với các phong cách đa dạng và miễn phí ship cho hoá đơn trên 2.000.000 VNĐ
-                    </small>
-                    <a href="" class="btn">Mua ngay &#8594;</a>
-                </div>
-            </div>
         </div>
     </div>
     <!-- ------------ testimonial -------------- -->
